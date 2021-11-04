0f9600db3b868d50c619de764d0e3e637426e78d
Dollfie <alexiel.nyo@gmail.com>
2021-11-04 18:43:14 -0300

    docu y tooltip

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,27 @@
+// Cambio de color nav bar
+
 $(window).scroll(function(){
     if ($(this).scrollTop() > 50) {
        $('#menu').addClass('active');
     } else {
        $('#menu').removeClass('active');
     }
+});
+
+
+// Smooth scrolling y tooltip
+$(function(){
+   $("a").click(function(event){
+      if(this.hash !==""){
+         event.preventDefault();
+         var gato = this.hash;
+         $("html, body").animate({
+            scrollTop: $(gato).offset().top
+         },800, function(){
+            window.location.hash = gato;
+         });
+      }
+
+   });
+   $('[data-toggle="tooltip"]').tooltip();
 });
